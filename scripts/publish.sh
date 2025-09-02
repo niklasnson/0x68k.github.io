0f437a65aaadae3bc169c2a6cf75166429cfc821
Niklas Nilsson <niklasnson@me.com>
2025-09-02 18:24:59 +0200

    working with the publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,7 +7,7 @@ last_SHA=$(git log --pretty=format:'%h' -n 1)
 tmp_dir="temp_$last_SHA"
 
 SITE="gh-pages"
-SOURCE="main" 
+SOURCE="main"
 
 # Build the Jekyll site directly to a temporary folder
 jekyll build -d ~/$tmp_dir > /dev/null 2>&1
@@ -29,9 +29,9 @@ git merge '@{u}' > /dev/null 2>&1
 # Remove the current contents of the SITE branch and
 #   replace them with the contents of the temp folder
 current_dir=${PWD}
-rm -r $current_dir/*
-git rm -r --cached * > /dev/null 2>&1
-cp -r ~/$tmp_dir/* $current_dir
+#rm -r $current_dir/*
+#git rm -r --cached * > /dev/null 2>&1
+#cp -r ~/$tmp_dir/* $current_dir
 
 # Commit the changes to the SITE branch
 message="Updated $SITE site from $SOURCE ($last_SHA)"
