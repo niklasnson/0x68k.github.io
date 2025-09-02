65799cfbc9ce84ab9cca967cacdd1f8c88372fd7
Niklas Nilsson <niklasnson@me.com>
2025-09-02 19:52:29 +0200

    *] Updated gh-pages site from main (03ce314)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,8 +9,9 @@ tmp_dir="temp_$last_SHA"
 SITE="gh-pages"
 SOURCE="main"
 
-echo "Publishing the blog to Github.com"
-echo "---------------------------------"
+echo " Publishing the blog to Github.com"
+echo " ---------------------------------"
+
 
 # Build the Jekyll site directly to a temporary folder
 jekyll build -d ~/$tmp_dir > /dev/null 2>&1
